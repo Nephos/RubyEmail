723351313b652072ff2d1015ac3af1d0cf10ff60
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-04-29 16:10:32 +0200

    improve tests, fix regexp, add helpers

--- lib/ruby_email.rb
+++ lib/ruby_email.rb
@@ -1,10 +1,30 @@
 module RubyEmail
+  # http://www.ietf.org/rfc/rfc5322.txt
 
-  ATEXT = '([\w!#\$%&\'*\+\-/=\?\^_`\{\}\|~])'
+  ATEXT = '([A-Za-z0-9!#\$%&\'*\+\-/=\?\^_`\{\}\|~])'
   #ATEXT = '\w'
   ATOM = "#{ATEXT}+"
-  DOT_ATOM_TEXT = "(#{ATOM})(\.#{ATOM})*"
+  DOT_ATOM_TEXT = "(#{ATOM})(\\.#{ATOM})*"
   VALIDE = "(?<local>#{DOT_ATOM_TEXT})@(?<domain>#{DOT_ATOM_TEXT})"
   REGEXP = Regexp.new "\\A#{VALIDE}\\Z"
 
+  def self.validates? str
+    !!match(str)
+  end
+
+  def self.match str
+    raise ArgumentError, "Cannot validates a `#{str.class}`. Only `String` can be." unless str.is_a?(String)
+    str.match(REGEXP)
+  end
+
+  module String
+    def is_email?
+      RubyEmail.validates? self
+    end
+  end
+
+end
+
+class String
+  include RubyEmail::String
 end
--- test/unit_test.rb
+++ test/unit_test.rb
@@ -7,24 +7,54 @@ require_relative '../lib/ruby_email'
 class TestRubyEmail < Test::Unit::TestCase
 
   def test_simple_true
-    assert "toto@toto".match RubyEmail::REGEXP
-    assert "toto@toto.toto".match RubyEmail::REGEXP
-    assert "toto@toto.toto.toto".match RubyEmail::REGEXP
-    assert "toto@toto.toto.toto.toto".match RubyEmail::REGEXP
+    assert RubyEmail.validates?("toto@toto")
+    assert RubyEmail.validates?("toto@toto.toto")
+    assert RubyEmail.validates?("toto@toto.toto.toto")
+    assert RubyEmail.validates?("toto@toto.toto.toto.toto")
   end
 
   def test_simple_false
-    assert_nil "t".match RubyEmail::REGEXP
-    assert_nil "t@".match RubyEmail::REGEXP
-    assert_nil "@t".match RubyEmail::REGEXP
+    assert !RubyEmail.validates?("t")
+    assert !RubyEmail.validates?("t@")
+    assert !RubyEmail.validates?("@t")
   end
 
-  def test_deep
-    m = "toto@toto.toto.toto.toto".match RubyEmail::REGEXP
-    #require 'pry';binding.pry
+  def test_plus_true
+    assert RubyEmail.validates?("i'am%toto@here")
+    assert RubyEmail.validates?("ich-bin-toto@___")
+    assert RubyEmail.validates?("arthur+spam@mail.com")
+  end
+
+  def test_plus_false
+    assert !RubyEmail.validates?("i have @ some things")
+    assert !RubyEmail.validates?("toto;toto@toto;toto")
+    assert !RubyEmail.validates?("toto,toto@toto,toto")
+    assert !RubyEmail.validates?("toto()toto@toto()toto")
+    assert !RubyEmail.validates?("toto[]toto@toto[]toto")
+    assert !RubyEmail.validates?("toto:toto@toto:toto")
+    assert !RubyEmail.validates?("toto<>toto@toto<>toto")
+    assert !RubyEmail.validates?("toto\\toto@toto\\toto")
+    assert !RubyEmail.validates?("toto\"toto@toto\"toto")
+  end
+
+  def test_match
+    m = RubyEmail.match "toto@toto.toto.toto.toto"
     assert m.names & %w(local domain)
     assert_equal "toto", m["local"]
     assert_equal "toto.toto.toto.toto", m["domain"]
+  end
+
+  def test_string
+    assert !"toto".is_email?
+    assert "toto@toto".is_email?
+  end
+
+  def test_exceptions
+    assert_raise { RubyEmail.match 1 }
+    assert_raise { RubyEmail.validates? 1.0 }
+    assert_raise { RubyEmail.validates? /toto/ }
+    assert_raise { RubyEmail.validates? :ok }
+    assert_raise { RubyEmail.validates? Class }
   end
 
 end
